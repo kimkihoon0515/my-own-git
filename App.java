dfd162c052c71bcfeb5b147cc555d2115a15ea1e
kimkihoon <rlarlgns0515@ajou.ac.kr>
2020-07-11 22:17:50 +0900

    Added Java code

---
+++ App.java
@@ -0,0 +1,14 @@
+package New.Git.Practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "This is my First JAVA code for git" );
+    }
+}

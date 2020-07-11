f530cfe477e9c461c921a1fed811bea1cf01fc7a
kimkihoon <rlarlgns0515@ajou.ac.kr>
2020-07-11 22:26:01 +0900

    App.java just updated

---
+++ App.java
@@ -0,0 +1,15 @@
+package New.Git.Practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "This is my First JAVA code for git" );
+        System.out.println(" Let's make our score better!");
+    }
+}
